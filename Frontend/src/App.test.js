a4aa33f97b7ceecf9d80c70b4e84f00f801d33bb
johnyan094 <sijunyan666@gmail.com>
2022-09-04 00:00:15 +1200

    fix package for not able to use user-event, add clear description unit test

--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import { AlertProvider } from './components/alert'
+import userEvent from '@testing-library/user-event'
 
 test('renders the footer text', () => {
   render(
@@ -10,4 +11,25 @@ test('renders the footer text', () => {
   )
   const footerElement = screen.getByText(/clearpoint.digital/i)
   expect(footerElement).toBeInTheDocument()
+})
+
+test('clear description', () => {
+  render(
+    <AlertProvider>
+      <App />
+    </AlertProvider>
+  )
+
+  // arrange
+  const descriptionValueElement = screen.getByPlaceholderText('Enter description...')
+  userEvent.type(descriptionValueElement, 'item 1')
+
+  const clearButton = screen.getByRole('button', {
+    name: /clear/i,
+  })
+
+  // act
+  expect(descriptionValueElement).toHaveValue('item 1')
+  userEvent.click(clearButton)
+  expect(descriptionValueElement).toHaveValue('')
 })
