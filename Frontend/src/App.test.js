a556f001032f03b4ad7a562c18b65152847a9f70
johnyan094 <sijunyan666@gmail.com>
2022-09-03 23:48:21 +1200

    fix unit test

--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { AlertProvider } from './components/alert'
 
 test('renders the footer text', () => {
-  render(<App />)
+  render(
+    <AlertProvider>
+      <App />
+    </AlertProvider>
+  )
   const footerElement = screen.getByText(/clearpoint.digital/i)
   expect(footerElement).toBeInTheDocument()
 })
